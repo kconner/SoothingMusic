fa5f0317ec87969a983897d1ebc368390b0d03da
Kevin Conner <connerk@gmail.com>
2016-12-28 10:46:49 -0500

    Initial Commit

---
+++ SoothingMusic WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SoothingMusic WatchKit Extension
+//
+//  Created by Kevin Conner on 12/28/16.
+//  Copyright © 2016 Kevin Conner. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
