f1a7c1e53fe039e4514529ca542606ec6f30a8c2
Kevin Conner <connerk@gmail.com>
2016-12-28 13:53:45 -0500

    Rebuild with AVFoundation instead of SceneKit.

--- SoothingMusic WatchKit Extension/InterfaceController.swift
+++ SoothingMusic WatchKit Extension/InterfaceController.swift
@@ -8,35 +8,57 @@
 
 import WatchKit
 import Foundation
-import SceneKit
+import AVFoundation
 
 final class InterfaceController: WKInterfaceController {
     
     @IBOutlet var sceneInterface: WKInterfaceSCNScene!
 
-    var player: SCNAudioPlayer!
-
-    private func setUp() {
-        guard let source = SCNAudioSource(named: "Larry Owens - Interlude.mp3"),
-            let scene = sceneInterface.scene,
-            let playerNode = scene.rootNode.childNode(withName: "player", recursively: true) else
-        {
-            preconditionFailure("RUINED")
-        }
-
-        source.loops = true
-        source.shouldStream = true
-
-        player = SCNAudioPlayer(source: source)
-        playerNode.addAudioPlayer(player)
-    }
+    private var engine: AVAudioEngine!
+    private var player: AVAudioPlayerNode!
+    private var audioFile: AVAudioFile!
 
     // MARK: WKInterfaceController
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        setUp()
+
+        engine = AVAudioEngine()
+
+        player = AVAudioPlayerNode()
+        engine.attach(player)
+
+        let stereoFormat = AVAudioFormat(standardFormatWithSampleRate: 44100.0, channels: 2)
+        engine.connect(player, to: engine.mainMixerNode, format: stereoFormat)
+
+        do {
+            let url = Bundle.main.url(forResource: "Larry Owens - Interlude", withExtension: "mp3")!
+            audioFile = try AVAudioFile(forReading: url)
+        } catch {
+            preconditionFailure("RUINED")
+        }
+
+        do {
+            if !engine.isRunning {
+                try engine.start()
+            }
+        } catch {
+            preconditionFailure("RUINED")
+        }
+
+        player.scheduleFile(audioFile, at: nil, completionHandler: nil)
+    }
+
+    override func willActivate() {
+        super.willActivate()
+
+        player.play()
+    }
+
+    override func didDeactivate() {
+        super.didDeactivate()
+
+        player.stop()
     }
 
 }
